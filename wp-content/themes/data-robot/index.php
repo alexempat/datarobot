bbd278eab3fe4c822977d5dc2c084e431a3ead6e
Alex_Orl <orlovskiy.job@gmail.com>
2021-04-19 19:03:21 +0300

    • Last fix 19.04

--- wp-content/themes/data-robot/index.php
+++ wp-content/themes/data-robot/index.php
@@ -15,32 +15,60 @@
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.4/lodash.min.js"></script><script src="js/script.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.10/lodash.min.js"></script>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class('fp'); ?>><?php wp_body_open(); ?>
 
-        <!--
-            You need to get raw data from a remote server( link ),
-            process it and then be able to output structured content in your template.  
-        -->  
+        <?php
+        $response = wp_remote_get('https://api.jsonbin.io/b/5dd7cefb040d843991f7183c');
+        $posts = json_decode(wp_remote_retrieve_body($response));
+        $departments = $posts->departments;
+        ?>
 
-        <div class="uk-container " >
-            <div class="fp-head uk-container">
+
+        <div class="uk-container " uk-filter="target: .js-filter">
+
+            <div class="fp-head uk-container uk-grid uk-position-relative">
                 <h1>Our open positions</h1>
+                <!-- Desktop -->
+                <div class="uk-position-top-right">
+                    <button class="fp-head__button" type="button">All departments</button>
+                    <div uk-dropdown="mode: click" style="display: none" uk-overflow-auto>
+                        <ul class="uk-nav uk-dropdown-nav">
+                            <li class="uk-active" uk-filter-control><a href="">All departments</a></li>
+                            <?php
+                            foreach ($departments as $tag) :
+                                $tag_stop = esc_html($tag->name);
+                                ?>
+                                <li uk-filter-control="[data-cat-filter*='<?php echo esc_html($tag->name); ?>']"><a href="#"><?php echo esc_html($tag->name); ?></a></li>
+                                <?php
+                            endforeach;
+                            ?>
+                        </ul>
+                    </div>
+
+                </div>
             </div>
-            <div class="fp-card uk-grid">
+
+            <ul class="fp-card uk-grid js-filter uk-position-relative">
 
                 <?php
-                $response = wp_remote_get('https://api.jsonbin.io/b/5dd7cefb040d843991f7183c');
-                $posts = json_decode(wp_remote_retrieve_body($response));
-                $departments = $posts->departments;
-                foreach ($posts->jobs as $post) {
 
+                foreach ($posts->jobs as $post) {
                     $department_id = $post->departments[0];
                     /*
                      * Block
                      */
-                    echo '<div class="fp-card__content" id="' . $post->id . '">';
+                    echo '<li class="fp-card__content" id="' . $post->id . '"';
+                    foreach ($departments as $department) {
+                        $name = $department->name;
+                        if ($department->id == $department_id) {
+                            echo 'data-cat-filter="' . esc_html($name) . '">';
+                        }
+                    }
                     // Top - Title
                     echo '<div class="fp-card__content-top">'
                     . '<a href="' . $post->absolute_url . '" target="_blank"><h3>' . $post->title . '</h3></a>'
@@ -61,16 +89,15 @@
                     . '</div>'; //End center block
                     // Bottom - Link
                     echo '<div class="fp-card__content-bottom">'
-                    . '<a href="' . $post->absolute_url . '" target="_blank">Learn More <img src="' . get_template_directory_uri() . '/assets/img/icon/arrow_blue.svg"></a>'
+                    . '<a href="' . $post->absolute_url . '" target="_blank">Learn More <img src="' . get_template_directory_uri() . '/assets/img/icon/arrow_blue.svg" uk-svg></a>'
                     . '</div>'; //End bottom block
 
-                    echo '</div>'; //End block
+                    echo '</li>'; //End block
                 }
                 ?> 
-
-            </div>
+            </ul>
         </div>
 
         <?php wp_footer(); ?>
-    </body>
+        </body>
 </html>
